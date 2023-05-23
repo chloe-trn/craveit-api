cff3c1e46e0f6465234a7a7b1916f83f44fcd052
Chloe <tran.m.chloe@gmail.com>
2023-05-23 17:31:29 -0400

    Add User controller and Registration method
    - Unit test added for a user registration POST request
    - Added POST request for user registration which calls the user service and confirmed it passed the unit test

---
+++ TasteBud.API/Controllers/UserController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TasteBud.API.Services.UserService;
+using TasteBud.API.ViewModels.Login;
+using TasteBud.API.ViewModels.Register;
+
+namespace TasteBud.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly IUserService _userService;
+
+        // Constructor of UserController
+        // Param: instance of IUserService
+        public UserController(IUserService userService)
+        {
+            _userService = userService;
+        }
+
+        // POST: api/user/register
+        // Endpoint for user registration
+        [HttpPost("register")]
+        public async Task<IActionResult> Register([FromBody] RegisterViewModel registerViewModel)
+        {
+            // Call UserService to register a new user
+            var result = await _userService.Register(registerViewModel);
+
+            // Return result of the registration process in an Ok response
+            return Ok(result);
+        }
+    }
+}
---
+++ TasteBud.UnitTests/ControllerTests/UserControllerTests.cs
@@ -0,0 +1,51 @@
+﻿using AutoFixture;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using TasteBud.API.Controllers;
+using TasteBud.API.Services.UserService;
+using TasteBud.API.ViewModels;
+using TasteBud.API.ViewModels.Register;
+
+namespace TasteBud.UnitTests.ControllerTests
+{
+    public class UserControllerTests
+    {
+        private UserController _userController;
+        private Mock<IUserService> _mockUserService; 
+        private Fixture _fixture;
+
+        public UserControllerTests()
+        {
+            _fixture = new Fixture();
+            _mockUserService = new Mock<IUserService>();
+        }
+
+        [Fact]
+        public async Task Post_Register_User_Returns_OK_With_Registration_Response_Body()
+        {
+            // Arrange
+            var registerViewModel = _fixture.Create<RegisterViewModel>();
+            var generalResponseViewModel = _fixture.Create<GeneralResponseViewModel>();
+            var generalResponseViewModelTask = Task.FromResult(generalResponseViewModel);
+
+            // Set up the mock user service to return the fake response task
+            _mockUserService.Setup(repo => repo.Register(It.IsAny<RegisterViewModel>()))
+                .Returns(generalResponseViewModelTask);
+
+            // Create the controller with the mock user service
+            _userController = new UserController(_mockUserService.Object);
+
+            // Act
+            var result = await _userController.Register(registerViewModel);
+            var obj = result as ObjectResult;
+
+            // Assert
+            Assert.Equal(200, obj.StatusCode);
+            Assert.IsType<GeneralResponseViewModel>(obj.Value);
+            Assert.Equal(generalResponseViewModel, obj.Value);
+        }
+
+        // TODO: User login unit test
+    }
+}
