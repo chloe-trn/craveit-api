64f36d561eaca6bec6ee66e05fe62d5319dc9add
Chloe <tran.m.chloe@gmail.com>
2023-05-23 17:28:34 -0400

    Create interface for user authentication service

---
+++ TasteBud.API/Services/UserService/IUserService.cs
@@ -0,0 +1,20 @@
+﻿using TasteBud.API.ViewModels;
+using TasteBud.API.ViewModels.Login;
+using TasteBud.API.ViewModels.Register;
+
+namespace TasteBud.API.Services.UserService
+{
+    // Interface that defines the necessary implementation for user authentication operations
+    public interface IUserService
+    {
+        // Definition: Performs user registration
+        // Param: RegisterViewModel 
+        // Return: a Task that represents an asynchronous operation and resolves to a RegisterResponseViewModel
+        Task<GeneralResponseViewModel> Register(RegisterViewModel registerViewModel);
+
+        // Definition: Performs user login
+        // Param: LoginViewModel 
+        // Return: a Task that represents an asynchronous operation and resolves to a LoginResponseViewModel
+        Task<LoginResponseViewModel> Login(LoginViewModel loginViewModel);
+    }
+}
