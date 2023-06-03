64fbd7d53fdc1ceb13ec4837d043a4c32deb4d13
Chloe <tran.m.chloe@gmail.com>
2023-06-03 13:21:49 -0400

    Add interfaces for the Result repository, Randomizer Service, and Yelp Service

---
+++ TasteBud.API/Services/RandomizerService/IRandomizerService.cs
@@ -0,0 +1,11 @@
+﻿using TasteBud.API.ViewModels.YelpViewModels;
+
+namespace TasteBud.API.Services.RandomizerService
+{
+    // Interface that defines the necessary implementation for a randomizer service
+    public interface IRandomizerService
+    {
+        // Gets a random item from a list of items
+        T GetRandomListItem<T>(List<T> items);
+    }
+}
---
+++ TasteBud.API/Services/YelpService/IYelpService.cs
@@ -0,0 +1,12 @@
+﻿using TasteBud.API.ViewModels.YelpViewModels;
+
+namespace TasteBud.API.Services.YelpService
+{
+    // Interface that defines the necessary implementation for external Yelp connection
+    public interface IYelpService
+    {
+        // Performs Yelp API call 
+        // TODO: Pass in a QuizResponseViewModel, use that information in the call
+        Task<YelpResponseViewModel> GetYelpData();
+    }
+}
