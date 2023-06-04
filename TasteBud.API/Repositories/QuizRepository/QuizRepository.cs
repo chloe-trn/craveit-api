d4040233d52f970532bc3c2f0e3cc826ecfb0a71
Chloe <tran.m.chloe@gmail.com>
2023-06-04 10:01:01 -0400

    Implement Quiz repository

---
+++ TasteBud.API/Repositories/QuizRepository/QuizRepository.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TasteBud.API.Data;
+using TasteBud.API.Models;
+
+namespace TasteBud.API.Repositories.QuizRepository
+{
+    public class QuizRepository : IQuizRepository
+    {
+        private readonly TasteBudApiDbContext _context;
+
+        public QuizRepository(TasteBudApiDbContext context)
+        {
+            _context = context;
+        }
+
+        // Retrieves a list of quizzes based on the authenticated user
+        public async Task<List<Quiz>> GetQuizzes(string authenticatedUserId)
+        {
+            return await _context.Quiz
+                .Include(u => u.Results)
+                .Where(q => q.UserId == authenticatedUserId)
+                .ToListAsync();
+        }
+
+        // Retrieves a specific quiz by its ID based on the authenticated user
+        public async Task<Quiz> GetQuizById(int id, string authenticatedUserId)
+        {
+            return await _context.Quiz
+                .FirstOrDefaultAsync(q => q.Id == id && q.UserId == authenticatedUserId);
+        }
+
+        // Adds a new quiz to the Quiz table
+        public async Task AddQuiz(Quiz quiz)
+        {
+            _context.Quiz.Add(quiz);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
