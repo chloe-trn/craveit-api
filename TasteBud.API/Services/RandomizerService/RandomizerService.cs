c2b674d39363d618f0a5710994034d826f136ce4
Chloe <tran.m.chloe@gmail.com>
2023-06-04 10:10:53 -0400

    Implement generic randomizer

---
+++ TasteBud.API/Services/RandomizerService/RandomizerService.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using TasteBud.API.Services.UserService;
+
+namespace TasteBud.API.Services.RandomizerService
+{
+    public class RandomizerService : IRandomizerService
+    {
+        private readonly Random _random;
+
+        public RandomizerService()
+        {
+            // Initialize the random number generator
+            _random = new Random();
+        }
+
+        // Method to get random item from a list of items
+        // A generic type parameter is used so a list of any type can be passed 
+        public T GetRandomListItem<T>(List<T> items)
+        {
+            // Generate a random index within the range of the items list
+            int randomIndex = _random.Next(0, items.Count);
+
+            // Retrieve the random item using the random index
+            T randomItem = items[randomIndex];
+
+            return randomItem;
+        }
+    }
+}
